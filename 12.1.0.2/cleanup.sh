3c9d817e17574f6ad26fff780891b948993f0da2
Pazus <pazzzus@gmail.com>
2017-08-15 21:05:10 +0300

    image v10

--- 12.1.0.2/cleanup.sh
+++ 12.1.0.2/cleanup.sh
@@ -1,5 +1,14 @@
+# Remove not needed components
 rm -rf $ORACLE_HOME/apex
-rm -rf $ORACLE_HOME/inventory/backup
+rm -rf $ORACLE_HOME/jdbc
+# ZDLRA installer files
+rm -rf $ORACLE_HOME/lib/ra*.zip
+rm -rf $ORACLE_HOME/ords
 rm -rf $ORACLE_HOME/sqldeveloper
-rm -rf $ORACLE_HOME/assistants/dbca/templates
-#mkdir -p $PDB_BASE_DIR
+rm -rf $ORACLE_HOME/ucp
+# OUI backup
+rm -rf $ORACLE_HOME/inventory/backup/*
+# Network tools help
+rm -rf $ORACLE_HOME/network/tools/help/mgr/help_*
+# Temp location
+rm -rf /tmp/* 
