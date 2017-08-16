ebe0fe6ee502f5d65926ecf6101f250deeabc666
Pazus <pazzzus@gmail.com>
2017-08-16 22:29:42 +0300

    12.2, 12.1 v10 images

--- 12.1.0.2/cleanup.sh
+++ 12.1.0.2/cleanup.sh
@@ -6,6 +6,8 @@ rm -rf $ORACLE_HOME/lib/ra*.zip
 rm -rf $ORACLE_HOME/ords
 rm -rf $ORACLE_HOME/sqldeveloper
 rm -rf $ORACLE_HOME/ucp
+# as we woun't install patches
+rm -rf $ORACLE_HOME/lib/*.a
 # OUI backup
 rm -rf $ORACLE_HOME/inventory/backup/*
 # Network tools help
