9770797b0c9fafb6ea8b17e159bda7f70406c3bc
Pazus <pazzzus@gmail.com>
2018-07-23 22:14:03 +0300

    start 18.3 image as copy of 12.2

---
+++ 18.3/cleanup.sh
@@ -0,0 +1,18 @@
+# Remove not needed components
+rm -rf $ORACLE_HOME/apex
+rm -rf $ORACLE_HOME/jdbc
+# ZDLRA installer files
+rm -rf $ORACLE_HOME/lib/ra*.zip
+rm -rf $ORACLE_HOME/ords
+rm -rf $ORACLE_HOME/sqldeveloper
+rm -rf $ORACLE_HOME/ucp
+# as we woun't install patches
+rm -rf $ORACLE_HOME/lib/*.a
+# OUI backup
+rm -rf $ORACLE_HOME/inventory/backup/*
+# Network tools help
+rm -rf $ORACLE_HOME/network/tools/help/mgr/help_*
+# Temp location
+rm -rf /tmp/* 
+#remove templates
+rm -rf $ORACLE_HOME/assistants/dbca/templates
