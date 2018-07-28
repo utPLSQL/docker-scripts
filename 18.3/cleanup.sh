1bebd3fb381f6d6ecfb70091303339107638afca
Pazus <pazzzus@gmail.com>
2018-07-28 13:34:29 +0300

    updated cleanup according to @gvenzl suggestions

--- 18.3/cleanup.sh
+++ 18.3/cleanup.sh
@@ -15,4 +15,16 @@ rm -rf $ORACLE_HOME/network/tools/help/mgr/help_*
 # Temp location
 rm -rf /tmp/* 
 #remove templates
+
+# Adviced by Gerald Venzl
+echo "Adviced by Gerald Venzl"
 rm -rf $ORACLE_HOME/assistants/dbca/templates
+rm -rf $ORACLE_HOME/.patch_storage/*
+rm -rf $ORACLE_HOME/R/*
+rm -rf $ORACLE_HOME/assistants/*
+rm -rf $ORACLE_HOME/cfgtoollogs/*
+rm -rf $ORACLE_HOME/dmu/*
+rm -rf $ORACLE_HOME/inventory/*
+rm -rf $ORACLE_HOME/javavm/*
+rm -rf $ORACLE_HOME/md/*
+rm -rf $ORACLE_HOME/suptools/*
